99df22d811581f032cdb9e68bd6608b2b9c0873b
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 01:11:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = 39.9999999999999
+var height = 39.99999999999999
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
